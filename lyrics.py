3804516e019280c42f526974c92135e4c190d2fe
John Tucker <johntuck4234@gmail.com>
2024-05-19 16:24:50 -0400

    Added lyrics to dataset, alongside get lyrics function

--- lyrics.py
+++ lyrics.py
@@ -4,13 +4,29 @@ import pandas as pd
 import numpy as np 
 import seaborn as sns 
 import os
-from dotenv import load_dotenv
 from lyricsgenius import Genius
 
 #Master list of beyonces songs, does not include Cowboy Carter
-master_Bey_list = pd.read_csv('beyonce_tracks.csv')
-print(master_Bey_list.head())
+Master_Bey_List = pd.read_csv('beyonce_tracks.csv')
+print(Master_Bey_List.head())
 
 #Genius API Access token found and APi defined
 genius_token = os.getenv('GENIUS_ACCESS_TOKEN')
 genius = Genius(genius_token)
+
+#Function to find lyrics from the Beyonce tracklist
+def get_lyrics(artist,song):
+    try:
+        track_lyrics = genius.search_song(song,artist)
+        if track_lyrics:
+            return track_lyrics.lyrics
+        else:
+            return "No lyrics found"
+    except TimeoutError:
+        return "Request timed out"
+    except Exception as e:
+        return "Error occured finding song lyrics"
+            
+if not os.path.isfile('beyonce_tracks_with_lyrics.csv'):
+    Master_Bey_List['lyrics'] = Master_Bey_List.apply(lambda row: get_lyrics(row['artist_name'], row['track_name']), axis=1)
+    Master_Bey_List.to_csv('beyonce_tracks_with_lyrics.csv', index=False)
