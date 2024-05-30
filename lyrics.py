b1e9ef93b6fb88e7f11dcdde97f59843bb0a63db
John Tucker <johntuck4234@gmail.com>
2024-05-30 19:51:56 -0400

    Added VADER Sentiment Analysis function and then add that into dataset

--- lyrics.py
+++ lyrics.py
@@ -6,6 +6,7 @@ import seaborn
 import os
 from lyricsgenius import Genius
 import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import google.generativeai as genai
 
 #Master list of beyonces songs, does not include Cowboy Carter
@@ -34,6 +35,7 @@ def get_lyrics(artist,song):
     except Exception as e:
         return "Error occured finding song lyrics"
     
+#GenAi Analyzes lyrics with the snetiment score and returns that
 def analyze_lyrics(lyrics,sentiment):
     lyric_analysis = lyric_model.generate_content("Analyze these lyrics: " + lyrics +" Alongside this sentiment score of", sentiment, ". Tell me the message behind the lyrics in this song.")
     return lyric_analysis.text
@@ -45,3 +47,14 @@ if not os.path.isfile('beyonce_tracks_with_lyrics.csv'):
 
 #Variable referencing new dataset
 Lyric_List = pd.read_csv('beyonce_tracks_with_lyrics.csv')
+
+#Download for nltk and then sentiment analysis
+nltk.download('all')
+
+#Grabs polarity from the
+def get_sentiment(lyrics):
+    sid = SentimentIntensityAnalyzer()
+    return sid.polarity_scores(lyrics)
+
+#Add sentiment score to dataset
+Lyric_List['sentiment'] = Lyric_List.apply(lambda row: get_sentiment(row['lyrics']))
