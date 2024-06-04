0dc6ba7c7697fa6659f04e7f8f478034a0bd86be
John Tucker <johntuck4234@gmail.com>
2024-06-03 22:22:56 -0400

    Created dataset with sentiment

--- lyrics.py
+++ lyrics.py
@@ -7,54 +7,57 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import google.generativeai as genai
 
 # #Master list of beyonces songs, does not include Cowboy Carter
-# Master_Bey_List = pd.read_csv('beyonce_tracks.csv')
-# print(Master_Bey_List.head())
+Master_Bey_List = pd.read_csv('beyonce_tracks.csv')
 
 # #Genius API Access token found and APi defined
-# genius_token = os.getenv('GENIUS_ACCESS_TOKEN')
-# genius = Genius(genius_token)
+genius_token = os.getenv('GENIUS_ACCESS_TOKEN')
+genius = Genius(genius_token)
 
 # #Establishing Gemini GenAI
-# genai_api_key = os.getenv('GOOGLE_API_KEY')
-# genai.configure(api_key=genai_api_key)
-# lyric_model = genai.GenerativeModel('gemini-pro')
+genai_api_key = os.getenv('GOOGLE_API_KEY')
+genai.configure(api_key=genai_api_key)
+lyric_model = genai.GenerativeModel('gemini-pro')
 
 # #Function to find lyrics from the Beyonce tracklist
-# def get_lyrics(artist,song):
-#     try:
-#         track_lyrics = genius.search_song(song,artist)
-#         if track_lyrics:
-#             return track_lyrics.lyrics
-#         else:
-#             return "No lyrics found"
-#     except TimeoutError:
-#         return "Request timed out"
-#     except Exception as e:
-#         return "Error occured finding song lyrics"
+def get_lyrics(artist,song):
+    try:
+        track_lyrics = genius.search_song(song,artist)
+        if track_lyrics:
+            return track_lyrics.lyrics
+        else:
+            return "No lyrics found"
+    except TimeoutError:
+        return "Request timed out"
+    except Exception as e:
+        return "Error occured finding song lyrics"
     
 # #GenAi Analyzes lyrics with the snetiment score and returns that
-# def analyze_lyrics(lyrics,sentiment):
-#     lyric_analysis = lyric_model.generate_content("Analyze these lyrics: " + lyrics +" Alongside this sentiment score of", sentiment, ". Tell me the message behind the lyrics in this song.")
-#     return lyric_analysis.text
+def analyze_lyrics(lyrics,sentiment):
+    lyric_analysis = lyric_model.generate_content("Analyze these lyrics: " + lyrics +" Alongside this sentiment score of", sentiment, ". Tell me the message behind the lyrics in this song.")
+    return lyric_analysis.text
             
 # #Add lyrics to dataset and create a new file for them. 
-# if not os.path.isfile('beyonce_tracks_with_lyrics.csv'):
-#     Master_Bey_List['lyrics'] = Master_Bey_List.apply(lambda row: get_lyrics(row['artist_name'], row['track_name']), axis=1)
-#     Master_Bey_List.to_csv('beyonce_tracks_with_lyrics.csv', index=False)
+if not os.path.isfile('beyonce_tracks_with_lyrics.csv'):
+    Master_Bey_List['lyrics'] = Master_Bey_List.apply(lambda row: get_lyrics(row['artist_name'], row['track_name']), axis=1)
+    Master_Bey_List.to_csv('beyonce_tracks_with_lyrics.csv', index=False)
 
 # #Variable referencing new dataset
 Lyric_List = pd.read_csv('beyonce_tracks_with_lyrics.csv')
 
 # #Download for nltk and then sentiment analysis
-# nltk.download('vader_lexicon')
+nltk.download('vader_lexicon')
 
-# #Grabs polarity from the
-# def get_sentiment(lyrics):
-#     sid = SentimentIntensityAnalyzer()
-#     return sid.polarity_scores(lyrics)
+#Grabs polarity from the lyrics
+def get_sentiment(lyrics):
+    sid = SentimentIntensityAnalyzer()
+    return sid.polarity_scores(lyrics)
 
-# #Add sentiment score to dataset
-# Lyric_List['sentiment'] = Lyric_List.apply(lambda row: get_sentiment(row['lyrics']))
+#Add sentiment score to dataset since it does not exist at first
+if 'sentiment' not in Lyric_List.columns.tolist():
+    Lyric_List['sentiment'] = Lyric_List.apply(lambda row: get_sentiment(row['lyrics']),axis=1)
+    Lyric_List.to_csv('beyonce_tracks_with_lyrics&sentiment.csv', index=False)
+
+Queens_List = pd.read_csv('beyonce_tracks_with_lyrics&sentiment.csv')
 
 #Function to find song from user input
 def find_song(user_input, dataset):
@@ -66,5 +69,3 @@ def find_song(user_input, dataset):
     else:
         return matches
     
-song = "kitty kat"
-print(find_song(song,Lyric_List))
